edd39482e52c2def2e668530a709b98d9d5deb4d
Mikołaj Chojnacki <mikolaj.chojnacki@wp.pl>
2023-06-30 16:23:45 +0200

    Add test Data

--- client/src/Components/Pages/Home/Home.tsx
+++ client/src/Components/Pages/Home/Home.tsx
@@ -1,10 +1,16 @@
 import { useState, useEffect, useRef } from 'react';
 import { getStockValues, getWigValues, } from '../../../UtilitieFunctions';
 import { STOCK, INDEX, INDEXES, WIG20_SHORCUTS } from '../../../Utilities';
-
+// trial ------------------------------------------------------------------------
+// ------------------------------------------------------------------------------
+import { TEST_STOCK_DATA, TEST_INDEXES_DATA } from '../../../TestDataUtilites';
+// end-trial --------------------------------------------------------------------
+// ------------------------------------------------------------------------------
 import styles from './Home.module.scss';
 
 
+// zmapowac dane percentageChange/valueChange na wartosc a nie tablice
+// sprawidzc mapwanie po errorez
 const Home = () => {
     const [loadProgress, setLoadProgress] = useState(0);
     const [indexesLoaded, setIndexesLoaded] = useState(false);
@@ -16,51 +22,60 @@ const Home = () => {
     const wigsContainer = useRef<HTMLDivElement>(null);
     const stocksContainer = useRef<HTMLDivElement>(null);
 
-    const downloadIndexPromise = (indexName: string) => {
+    const createDownloadPromise = (itemName: string, getFunction: Function) => {
         return new Promise(async (resolve, reject) => {
-            const value = await getWigValues(indexName)
+            const value = await getFunction(itemName)
             setLoadProgress(prev => prev + 4)
             resolve(value)
         })
     }
 
-    const downloadStockPromise = (stockName: string) => {
-        return new Promise(async (resolve, reject) => {
-            const value = await getStockValues(stockName)
-            setLoadProgress(prev => prev + 4)
-            resolve(value)
+    const createIndexesPromiseArray = () => {
+        let promisesArray: any[] = [];
+
+        INDEXES.forEach(async (indexName: string) => {
+            promisesArray.push(createDownloadPromise(indexName, getWigValues))
+        })
+        return promisesArray
+    }
+
+    const createStocksPromiseArray = () => {
+        let promisesArray: any[] = [];
+
+        WIG20_SHORCUTS.forEach((company: { name: string; shortcut: string }) => {
+            promisesArray.push(createDownloadPromise(company.shortcut, getStockValues))
+        })
+        return promisesArray
+    }
+
+    const formatData = (data: any[]): any[] => {
+        return data.map((item) => {
+            if (item.error) return item;
+            return {
+                ...item,
+                percentageChange: item.percentageChange[0],
+                valueChange: item.valueChange[0]
+            }
         })
     }
 
     const downloadIndexes = async () => {
+        let indexesValues = await Promise.all(createIndexesPromiseArray())
 
-        let promisesArray: any[] = [];
-        INDEXES.forEach(async (indexName: string) => {
-            promisesArray.push(downloadIndexPromise(indexName))
-        })
-
-        const indexesValues = await Promise.all(promisesArray)
-
+        indexesValues = formatData(indexesValues)
         setIndexes(indexesValues)
         setIndexesLoaded(true)
     }
 
     const downloadStocks = async () => {
+        let stocksValues = await Promise.all(createStocksPromiseArray())
 
-        let promisesArray: any[] = [];
-        WIG20_SHORCUTS.forEach((company: { name: string; shortcut: string }) => {
-            promisesArray.push(downloadStockPromise(company.shortcut))
-        })
-
-        const stocksValues = await Promise.all(promisesArray)
-
+        stocksValues = formatData(stocksValues)
         setStocks(stocksValues)
         setStocksLoaded(true)
-
     }
 
     const showIndexes = () => {
-        // display elements (opacity 0)
         wigsContainer.current?.setAttribute(`style`, `display: block;`);
         stocksContainer.current?.setAttribute(`style`, `display: block;`);
         // wait moment and start displaying
@@ -71,10 +86,41 @@ const Home = () => {
 
     }
 
+    // trial ------------------------------------------------------------------------
+    // ------------------------------------------------------------------------------
+    const getTestData = () => {
+        setStocks(TEST_STOCK_DATA)
+        setIndexes(TEST_INDEXES_DATA)
+    }
+    // end-trial --------------------------------------------------------------------
+    // ------------------------------------------------------------------------------
+
+
     useEffect(() => {
-        downloadIndexes()
-        downloadStocks()
+        // downloadIndexes()
+        // downloadStocks()
+
+        // trial ------------------------------------------------------------------------
+        // ------------------------------------------------------------------------------
+        getTestData()
+        setIndexesLoaded(true)
+        setStocksLoaded(true)
+        // end-trial --------------------------------------------------------------------
+        // ------------------------------------------------------------------------------
     }, [])
+
+
+
+    // trial ------------------------------------------------------------------------
+    // ------------------------------------------------------------------------------
+    // useEffect(() => {
+    //     console.log(stocks);
+    //     console.log(indexes);
+    // }, [stocks, indexes])
+    // end-trial --------------------------------------------------------------------
+    // ------------------------------------------------------------------------------
+
+
 
     return (
         <div className={styles.container}>
