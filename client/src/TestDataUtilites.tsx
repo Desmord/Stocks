f6480ec753ade89b55a78845757b26664b97f010
Mikołaj Chojnacki <mikolaj.chojnacki@wp.pl>
2023-07-09 20:20:11 +0200

    Create transactions test data

--- client/src/TestDataUtilites.tsx
+++ client/src/TestDataUtilites.tsx
@@ -253,3 +253,400 @@ export const TEST_INDEXES_DATA: INDEX[] = [
 
     }
 ]
+
+export const TEST_TRANSACTIONS = [
+    {
+        id: "1",
+        shortcut: "PLAYWAY",
+        name: "PlayWay SA",
+        type: "stock",
+        // type: "stock, feature, option, divident",
+        acqusition: true,
+        disposal: false,
+        quantity: 11,
+        price: 261.50,
+        commision: 11.22,
+        totalPrice: 2887.72,
+        commisionFreePrice:2876.5,
+        totalProfit: 0,             // if acuisition 0 ( and not divident? )
+        profit:0,                   // if acuisition 0 ( and not divident? )
+        totalLoss: 0 ,                // if acuisition 0   
+        loss: 0,                    // if acusition 0
+        totalPercentageChange:0,    // if acusition 0
+        percentageChange: 0,        // if acusition 0
+        date: "2022-05-20",
+        notes: "Probna notka do transakcji payway",
+        transactionReason: "Brak",
+        group: [
+            "Grupa test 1"
+        ],
+        divident: {
+            date: "0",
+            totalProfit: 0,
+            name: "",
+            profit: 0,
+            percentageProfit: 0
+        }
+    },
+    {
+        id: "2",
+        shortcut: "PLAYWAY",
+        name: "PlayWay SA",
+        type: "stock",
+        acqusition: false,
+        disposal: true,
+        quantity: 11,
+        price: 390.00,
+        commision: 16.73,
+        totalPrice: 4306.73,
+        commisionFreePrice:4290,
+        totalProfit: 1419.01,         // if acuisition 0 ( and not divident? )
+        profit:1385.55,               // if acuisition 0 ( and not divident? )
+        totalLoss: 0 ,                // if acuisition 0   
+        loss: 0,                      // if acusition 0
+        totalPercentageChange:67,      // if acusition 0
+        percentageChange: 48,          // if acusition 0
+        date: "2022-05-25",
+        notes: "Probna notka do transakcji payway",
+        transactionReason: "Brak",
+        group: [
+            "Grupa test 1"
+        ],
+        divident: {
+            date: "0",
+            totalProfit: 0,
+            name: "",
+            profit: 0,
+            percentageProfit: 0
+        }
+    },
+    {
+        id: "3",
+        shortcut: "11BIT",
+        name: "11 bit studios",
+        type: "stock",
+        // type: "stock, feature, option, divident",
+        acqusition: true,
+        disposal: false,
+        quantity: 3,
+        price: 544,
+        commision: 6.36,
+        totalPrice: 1638.36,
+        commisionFreePrice:1632,
+        totalProfit: 0,             // if acuisition 0 ( and not divident? )
+        profit:0,                   // if acuisition 0 ( and not divident? )
+        totalLoss: 0 ,                // if acuisition 0   
+        loss: 0,                    // if acusition 0
+        totalPercentageChange:0,    // if acusition 0
+        percentageChange: 0,        // if acusition 0
+        date: "2022-06-20",
+        notes: "Probna notka do transakcji 11bit",
+        transactionReason: "Brak",
+        group: [
+            "Grupa test 1"
+        ],
+        divident: {
+            date: "0",
+            totalProfit: 0,
+            name: "",
+            profit: 0,
+            percentageProfit: 0
+        }
+    },
+    {
+        id: "4",
+        shortcut: "11BIT",
+        name: "11 bit studios",
+        type: "stock",
+        // type: "stock, feature, option, divident",
+        acqusition: false,
+        disposal: true,
+        quantity: 3,
+        price: 553.5,
+        commision: 6.48,
+        totalPrice: 1666.98,
+        commisionFreePrice:1660.5,
+        totalProfit: 28.62,             // if acuisition 0 ( and not divident? )
+        profit:15.66,                   // if acuisition 0 ( and not divident? )
+        totalLoss: 0 ,                // if acuisition 0   
+        loss: 0,                        // if acusition 0
+        totalPercentageChange:1.01,        // if acusition 0
+        percentageChange: 0.96,        // if acusition 0
+        date: "2022-06-29",
+        notes: "Probna notka do transakcji 11bit",
+        transactionReason: "Brak",
+        group: [
+            "Grupa test 1"
+        ],
+        divident: {
+            date: "0",
+            totalProfit: 0,
+            name: "",
+            profit: 0,
+            percentageProfit: 0
+        }
+    },
+    {
+        id: "5",
+        shortcut: "ARTIFEX",
+        name: "Artifex Mundi SA",
+        type: "stock",
+        // type: "stock, feature, option, divident",
+        acqusition: true,
+        disposal: false,
+        quantity: 293,
+        price: 9.60,
+        commision: 10.97,
+        totalPrice: 2823.77,
+        commisionFreePrice:2812.8,
+        totalProfit: 0,             // if acuisition 0 ( and not divident? )
+        profit:0,                   // if acuisition 0 ( and not divident? )
+        totalLoss: 0 ,                // if acuisition 0   
+        loss: 0,                    // if acusition 0
+        totalPercentageChange:0,    // if acusition 0
+        percentageChange: 0,        // if acusition 0
+        date: "2023-03-10",
+        notes: "Probna notka do transakcji Artifex",
+        transactionReason: "DM TRIGON Kacper kapron rekomendacja",
+        group: [
+            "Grupa test 1"
+        ],
+        divident: {
+            date: "0",
+            totalProfit: 0,
+            name: "",
+            profit: 0,
+            percentageProfit: 0
+        }
+    },
+    {
+        id: "6",
+        shortcut: "ARTIFEX",
+        name: "Artifex Mundi SA",
+        type: "stock",
+        // type: "stock, feature, option, divident",
+        acqusition: false,
+        disposal: true,
+        quantity: 293,
+        price: 12.40,
+        commision: 14.17,
+        totalPrice: 3647.37,
+        commisionFreePrice:3633.2,
+        totalProfit: 823.6,             // if acuisition 0 ( and not divident? )
+        profit:795.26,                   // if acuisition 0 ( and not divident? )
+        totalLoss: 0 ,                // if acuisition 0   
+        loss: 0,                    // if acusition 0
+        totalPercentageChange:30,    // if acusition 0
+        percentageChange: 28.16,        // if acusition 0
+        date: "2023-04-11",
+        notes: "Probna notka do transakcji Artifex",
+        transactionReason: "DM TRIGON Kacper kapron rekomendacja",
+        group: [
+            "Grupa test 1"
+        ],
+        divident: {
+            date: "0",
+            totalProfit: 0,
+            name: "",
+            profit: 0,
+            percentageProfit: 0
+        }
+    },
+    {
+        id: "7",
+        shortcut: "ALLEGRO",
+        name: "Allegro",
+        type: "stock",
+        // type: "stock, feature, option, divident",
+        acqusition: true,
+        disposal: false,
+        quantity: 22,
+        price: 84.93,
+        commision: 7.26,
+        totalPrice: 1875.72,
+        commisionFreePrice:1868.46,
+        totalProfit: 0,             // if acuisition 0 ( and not divident? )
+        profit:0,                   // if acuisition 0 ( and not divident? )
+        totalLoss: 0 ,                // if acuisition 0   
+        loss: 0,                    // if acusition 0
+        totalPercentageChange:0,    // if acusition 0
+        percentageChange: 0,        // if acusition 0
+        date: "2023-01-10",
+        notes: "Probna notka do transakcji Allegro",
+        transactionReason: "DM TRIGON Kacper kapron rekomendacja",
+        group: [
+            "Grupa test 2"
+        ],
+        divident: {
+            date: "0",
+            totalProfit: 0,
+            name: "",
+            profit: 0,
+            percentageProfit: 0
+        }
+    },
+    {
+        id: "8",
+        shortcut: "ALLEGRO",
+        name: "Allegro",
+        type: "stock",
+        // type: "stock, feature, option, divident",
+        acqusition: false,
+        disposal: true,
+        quantity: 22,
+        price: 73.55,
+        commision: 6.31,
+        totalPrice: 1624.41,
+        commisionFreePrice:1618.1,
+        totalProfit: 0,             // if acuisition 0 ( and not divident? )
+        profit:0,                   // if acuisition 0 ( and not divident? )
+        totalLoss: 257.62,                // if acuisition 0   
+        loss: 244.05,                    // if acusition 0
+        totalPercentageChange:14.5,    // if acusition 0
+        percentageChange: 13.5,        // if acusition 0
+        date: "2023-02-24",
+        notes: "Probna notka do transakcji Allegro",
+        transactionReason: "DM TRIGON Kacper kapron rekomendacja",
+        group: [
+            "Grupa test 2"
+        ],
+        divident: {
+            date: "0",
+            totalProfit: 0,
+            name: "",
+            profit: 0,
+            percentageProfit: 0
+        }
+    },
+    {
+        id: "9",
+        shortcut: "COMARCH",
+        name: "Comarch SA",
+        type: "stock",
+        // type: "stock, feature, option, divident",
+        acqusition: true,
+        disposal: false,
+        quantity: 17,
+        price: 197.5,
+        commision: 13.09,
+        totalPrice: 3370.59,
+        commisionFreePrice:3357.5,
+        totalProfit: 0,             // if acuisition 0 ( and not divident? )
+        profit:0,                   // if acuisition 0 ( and not divident? )
+        totalLoss: 0 ,                // if acuisition 0   
+        loss: 0,                    // if acusition 0
+        totalPercentageChange:0,    // if acusition 0
+        percentageChange: 0,        // if acusition 0
+        date: "2023-01-20",
+        notes: "Probna notka do transakcji Cormach",
+        transactionReason: "Brak",
+        group: [
+            "Grupa test 1"
+        ],
+        divident: {
+            date: "0",
+            totalProfit: 0,
+            name: "",
+            profit: 0,
+            percentageProfit: 0
+        }
+    },
+    {
+        id: "10",
+        shortcut: "COMARCH",
+        name: "Comarch SA",
+        type: "stock",
+        // type: "stock, feature, option, divident",
+        acqusition: false,
+        disposal: true,
+        quantity: 17,
+        price: 196,
+        commision: 12.99,
+        totalPrice:3332,
+        commisionFreePrice:3319.01,  // w przypadku sprzedazy to komision dodajemy tutaj
+        totalProfit: 0,             // if acuisition 0 ( and not divident? )
+        profit:0,                   // if acuisition 0 ( and not divident? )
+        totalLoss: 51.58 ,                // if acuisition 0   
+        loss: 25.5,                    // if acusition 0
+        totalPercentageChange:1.78,    // if acusition 0
+        percentageChange: 1.51,        // if acusition 0
+        date: "2023-01-28",
+        notes: "Probna notka do transakcji Cormach",
+        transactionReason: "Brak",
+        group: [
+            "Grupa test 2"
+        ],
+        divident: {
+            date: "0",
+            totalProfit: 0,
+            name: "",
+            profit: 0,
+            percentageProfit: 0
+        }
+    },
+    {
+        id: "11",
+        shortcut: "CDPROJEKT",
+        name: "CD Projekt SA",
+        type: "stock",
+        // type: "stock, feature, option, divident",
+        acqusition: true,
+        disposal: false,
+        quantity: 31,
+        price: 149.15,
+        commision: 18.03,
+        totalPrice: 4641.68,
+        commisionFreePrice:4623.65,
+        totalProfit: 0,             // if acuisition 0 ( and not divident? )
+        profit:0,                   // if acuisition 0 ( and not divident? )
+        totalLoss: 0 ,                // if acuisition 0   
+        loss: 0,                    // if acusition 0
+        totalPercentageChange:0,    // if acusition 0
+        percentageChange: 0,        // if acusition 0
+        date: "2023-05-07",
+        notes: "Probna notka do transakcji CD Projekt",
+        transactionReason: "Brak",
+        group: [
+            "Grupa test 2"
+        ],
+        divident: {
+            date: "0",
+            totalProfit: 0,
+            name: "",
+            profit: 0,
+            percentageProfit: 0
+        }
+    },
+]
+
+
+    // try catch
+    // const clickTest = async () => {
+
+    //     const response = await fetch(`http://localhost:8000/api/dataBase/editUser`, {
+    //         method: `POST`,
+    //         mode: "cors",
+    //         headers: {
+    //             "Content-Type": "application/json",
+    //         },
+    //         body: JSON.stringify({
+    //             id: `64a466aa67804a64c96bcfa0`,
+    //             login: `mikolaj`,
+    //             password: `haslo`,
+    //             tips: [
+    //                 "Kupować na początku kwietnia i sprzedawać po koniec sierpnia. Najważniejsze",
+    //                 "Kupować na początku stycznia i sprzedawać na konću. Ważne.",
+    //                 "Kupować na początku grudnia i sprzedawać na koncu stycznia. Mniej ważne.",
+    //                  "Nie moze byc strzsze zle ck ..."
+    //                  "czy ma diwidente dodatkowy plus"
+    //                  "Powyzej jakiejs kwoty jest niska prowizja"
+//                         "fundamenty wazniejsze niz techniczna ktora tylko do wsparcia"
+    //             ],
+    //             transactions: [  
+    //                  ...
+    //             ],
+    //         })
+    //     });
+
+    //     console.log(await response.json())
+    // }
